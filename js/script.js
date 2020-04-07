3bfb32433841de7ec9b1e163b0df76090d8f4d27
ss-stolyarova <61242557+ss-stolyarova@users.noreply.github.com>
2020-04-07 18:34:42 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,6 +12,79 @@ window.addEventListener('DOMContentLoaded', () => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
             menu.classList.toggle('menu_active');
-        })
-    })
-})
+        });
+    });
+});
+
+$(document).ready(function(){
+    
+    $('.btn_order').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal-body__descr').text($('.catalog__card__subtitle').eq(i).text());
+        });
+    });
+
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+            name: "required",
+            phone: "required",      
+            },
+            messages: {
+                name: "Пожалуйста, введите свое имя",
+                phone: "Пожалуйста, введите свой номер телефона",
+            }
+        });
+    };
+    validateForms('#promo-form');
+    validateForms('#order-form');
+
+    $('input[name=phone]').mask("+7-999-999-99-99");
+
+
+/*     $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('form').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });   */  
+    
+     $('form').submit(function() {
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('form').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });   
+ 
+
+    //smooth scroll and page up
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+});
